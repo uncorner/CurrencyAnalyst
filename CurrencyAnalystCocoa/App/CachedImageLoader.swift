17da64dcd9669b1f79590dbcbcd05fed2146650a
uncorner <uncorner.box@gmail.com>
2021-09-09 20:25:11 +0300

    download image by AF

--- CurrencyAnalystCocoa/App/CachedImageLoader.swift
+++ CurrencyAnalystCocoa/App/CachedImageLoader.swift
@@ -8,27 +8,20 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
 class CachedImageLoader {
-    
-    var task: URLSessionDownloadTask!
-    var session: URLSession!
-    var cache: NSCache<NSString, UIImage>!
-    
-    init() {
-        session = URLSession.shared
-        task = URLSessionDownloadTask()
-        self.cache = NSCache()
-    }
+    var cache: NSCache<NSString, UIImage> = NSCache()
     
     func getImage(imageUrl: URL, completion: @escaping (UIImage?, URL) -> ()) {
         let imagePath = imageUrl.absoluteString
         
         if let image = cache.object(forKey: imagePath as NSString) {
+            // get from cache
             completion(image, imageUrl)
         } else {
-            task = session.downloadTask(with: imageUrl, completionHandler: { (location, response, error) in
-                if error == nil, let data = try? Data(contentsOf: imageUrl) {
+            AF.request(imageUrl).responseData { response in
+                if response.error == nil, let data = response.value {
                     if let image = UIImage(data: data) {
                         // set cache
                         self.cache.setObject(image, forKey: imagePath as NSString)
@@ -41,10 +34,9 @@ class CachedImageLoader {
                 else {
                     completion(nil, imageUrl)
                 }
-            })
-            
-            task.resume()
+            }
         }
+        
     }
     
 }
